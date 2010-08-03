7037a73d227f4d8d890c7b5cf85465b9e62bf7da
Henning Günther <h.guenther@tu-bs.de>
2010-08-03 12:33:45 +0200

    Make scheduling optional via CMake variable

--- plugin.c
+++ plugin.c
@@ -12,28 +12,36 @@ pid_t dispatch_plugin(const plugin_t* plugin,rule_list_t* rules,classification_c
     double* vec = calloc(plugin->feature_vector_size+1,sizeof(double));
     char* eval_str;
     double raw;
+#ifdef SCHEDULING
     scheduler_t sched;
     int idle_counter = 0;
     scheduler_init(&sched);
+#endif
     asprintf(&eval_str,"%s evaluation",plugin->name);
     vec[plugin->feature_vector_size] = 0.0;
     while(*running) {
+#ifdef SCHEDULING
       if(idle_counter >= scheduler_get_rate(&sched) || idle_counter >= 20) {
+        idle_counter = 0;
+#endif
         if(plugin->callback(plugin->user_data,vec) == 0) {
-          idle_counter = 0;
           MEASURED(eval_str,
                    { rules = evaluate_classifier(rules,vec,&class,&raw); });
           vec[plugin->feature_vector_size] = raw;
+#ifdef SCHEDULING
           scheduler_add_context(&sched,class);
+#endif
           printf("%s (%f)\n",class,raw);
           cb(class,raw,cb_data);
         } else {
-          idle_counter++;
+          fprintf(stderr,"WARNING: plugin %s failed to produce feature vector\n",plugin->name);
         }
+#ifdef SCHEDULING
       } else {
         plugin->skipper(plugin->user_data);
         idle_counter++;
       }
+#endif
     }
     free(vec);
     free(eval_str);
