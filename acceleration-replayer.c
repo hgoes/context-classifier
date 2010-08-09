68114e1e52ba53de8e797ee30101339f36ed12cd
Henning Günther <h.guenther@tu-bs.de>
2010-08-09 16:45:42 +0200

    Use both sensors as input for one data package in acceleration replayer

--- acceleration-replayer.c
+++ acceleration-replayer.c
@@ -1,17 +1,21 @@
 #include "acceleration-replayer.h"
 
 static int read_acceleration_package(char** str,double* vec,struct timeval* tp) {
-  double data[8][3];
+  double data[8][6];
   long int ts_secs,ts_usecs = 0;
   char ts_fracs[10];
   char context[20];
-  int i;
+  int i,j;
   int fac;
   for(i=0;i<8;i++) {
-    int res = sscanf(str[0],"%ld.%9[0123456789] %s %*s %lf %lf %lf",&ts_secs,ts_fracs,context,&data[i][0],&data[i][1],&data[i][2]);
-    if(res != 6) {
-      context[0] = '\0';
-      sscanf(str[0],"%ld.%9[0123456789] %*s %lf %lf %lf",&ts_secs,ts_fracs,&data[i][0],&data[i][1],&data[i][2]);
+    for(j=0;j<6;j+=3) {
+      int res = sscanf(str[0],"%ld.%9[0123456789] %s %*s %lf %lf %lf",&ts_secs,ts_fracs,context,
+                       &data[i][j],&data[i][j+1],&data[i][j+2]);
+      if(res != 6) {
+        context[0] = '\0';
+        sscanf(str[0],"%ld.%9[0123456789] %*s %lf %lf %lf",&ts_secs,ts_fracs,
+               &data[i][j],&data[i][j+1],&data[i][j+2]);
+      }
     }
   }
   fac = 100000;
@@ -20,32 +24,40 @@ static int read_acceleration_package(char** str,double* vec,struct timeval* tp) 
     ts_usecs += ((int)(ts_fracs[i] - '0'))*fac;
     fac /= 10;
   }
-  double sx=0.0,sy=0.0,sz=0.0;
-  for(i=0;i<8;i++) {
-    sx+=data[i][0];
-    sy+=data[i][1];
-    sz+=data[i][2];
+  double means[6];
+  for(j=0;j<6;j++) {
+    means[j]=0.0;
   }
-  sx /= 8.0;
-  sy /= 8.0;
-  sz /= 8.0;
-
-  double vx=0.0,vy=0.0,vz=0.0;
   for(i=0;i<8;i++) {
-    double t;
-    t = data[i][0]-sx;
-    vx += t*t;
-    t = data[i][1]-sy;
-    vy += t*t;
-    t = data[i][2]-sz;
-    vz += t*t;
+    for(j=0;j<6;j++) {
+      means[j]+=data[i][j];
+    }
   }
-  vec[0] = vx / 8.0;
-  vec[1] = vy / 8.0;
-  vec[2] = vz / 8.0;
-  vec[3] = sx;
-  vec[4] = sy;
-  vec[5] = sz;
+  for(j=0;j<6;j++) {
+    means[j] /= 8.0;
+  }
+  double vars[6];
+  for(j=0;j<6;j++) {
+    vars[j]=0.0;
+  }
+  for(i=0;i<8;i++) {
+    for(j=0;j<6;j++) {
+      double t = data[i][j]-means[j];
+      vars[j]+=t*t;
+    }
+  }
+  vec[0] = vars[0] / 8.0;
+  vec[1] = vars[1] / 8.0;
+  vec[2] = vars[2] / 8.0;
+  vec[3] = means[0];
+  vec[4] = means[1];
+  vec[5] = means[2];
+  vec[6] = vars[3] / 8.0;
+  vec[7] = vars[4] / 8.0;
+  vec[8] = vars[5] / 8.0;
+  vec[9] = means[3];
+  vec[10] = means[4];
+  vec[11] = means[5];
 
   tp->tv_sec = ts_secs;
   tp->tv_usec = ts_usecs;
@@ -53,5 +65,5 @@ static int read_acceleration_package(char** str,double* vec,struct timeval* tp) 
 }
 
 plugin_t* get_acceleration_replayer_plugin(const char* fn) {
-  return get_replayer_plugin(fn,6,16,read_acceleration_package);
+  return get_replayer_plugin(fn,12,32,read_acceleration_package);
 }
